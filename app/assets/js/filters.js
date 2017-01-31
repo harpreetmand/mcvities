8e429e01cb120d4ed62ae8ebc1ad5b37f00e5794
Harpreet Mand <harpreet@limeadvertising.com>
2017-01-31 11:42:30 -0500

    our recipe on home page shows videos first and then shuffles it. also added the correct sass files

--- app/assets/js/filters.js
+++ app/assets/js/filters.js
@@ -45,3 +45,39 @@ app.filter('shuffle', function() {
 	    }
     };
 });
+
+app.filter('videosFirst', function(){
+    var newArr = [];
+    var count = 0;
+    return function(arr){
+        if(arr!=undefined){
+            if(count == 0){
+               for(var i = 0; i < arr.length; i++){
+                   if(arr[i].video != undefined){
+                       newArr.push(arr[i]);
+                   }        
+                }
+                var tillFive = 5 - newArr.length;
+                for(var i = 0; i < tillFive; i++){
+                    newArr.push(arr[i]);
+                    count++;
+                }    
+                //                  var i = 0
+                // , j = 0
+                // , temp = null
+
+                // for (i = newArr.length - 1; i > 0; i -= 1) {
+                //     j = Math.floor(Math.random() * (i + 1))
+                //     temp = newArr[i]
+                //     newArr[i] = newArr[j]
+                //     newArr[j] = temp
+                // }
+                // if(newArr.length > 4){
+                //     newArr.splice(5, (newArr.length -5));
+                // }           
+            }
+            arr = newArr;
+            return arr;
+        }
+    }
+});
